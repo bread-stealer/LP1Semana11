b21935c03ceb64cc47cf1ea446c0158151e9d330
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-05-21 16:09:01 +0100

    Move public class generator to Model.cs

---
+++ StringGenerator/Model.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+namespace StringGenerator
+{
+    public class Model
+    {
+        private const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        
+        public static string Generate(int seed, int length = 16)
+        {
+            Random rng = new Random(seed);
+
+            StringBuilder result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rng.Next(chars.Length);
+                result.Append(chars[index]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
